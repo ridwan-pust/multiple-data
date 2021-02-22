5bb923929cd4c273a93b2899fa1d6cad09eca277
ridwan-pust <73843013+ridwan-pust@users.noreply.github.com>
2021-02-22 08:10:20 +0600

    Add files via upload

---
+++ CoursesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllPrackticsUsingCore.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public IActionResult Create(List<Course> courses)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUser(Member member, string[] bat,string[] ball, bool[] check, int[] chekvalue)
+        {
+            if (ModelState.IsValid)
+            {
+
+
+                //_context.AddRange(member);
+                //await _context.SaveChangesAsync();
+
+                //return RedirectToAction(nameof(Index));
+            }
+            return View(member);
+        }
+    }
+}
